914ab518411d86176668e870d3cc5ec89b0ac1f2
hali <88402732+husnainali01@users.noreply.github.com>
2022-08-16 07:40:45 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ namespace VSCodeNetTest
+ {
+    class program
+    {
+     static void Main(string[] args)
+     {
+        LinkedList<string> Arr = new LinkedList<string>();
+        Arr.AddFirst("Husnain");
+        Arr.AddLast("Ali");
+        Arr.AddLast("Asghar");
+         foreach(var temp in Arr)
+        {
+              Console.WriteLine(temp);
+        }
+     }
+    }
+ }
