4a5bf775c4a74c4dac829011f407dc6191b74315
hali <88402732+husnainali01@users.noreply.github.com>
2022-08-16 05:25:17 -0700

    Add files via upload

---
+++ First Program/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+ namespace VSCodeNetTest
+ {
+    class program
+    {
+     static void Main(string[] args)
+     {
+        Console.WriteLine("Hello World! \n Husnain Ali Asghar");
+     }
+    }
+ }
